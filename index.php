cdd271071b52c884206a16cef8baa3cd8c1929c8
Richard Neish <richardn@richardneish.org>
2015-07-26 23:33:08 +0100

    Rudimentary GET interface.

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';  // Composer autoload
 
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app->contentType('application/json');
+$db = new PDO('sqlite:data/database.sqlite');
+
+// Routes
+$app->get('/electricity_meter_reading', function () use ($db, $app) {
+  $sth = $db->query('SELECT * FROM electricity_meter_reading;');
+  echo json_encode($sth->fetchAll(PDO::FETCH_CLASS));
+});
+
+$app->get('/electricity_meter_reading/:id', function ($id) use ($db, $app) {
+  $sth = $db->prepare('SELECT * FROM electricity_meter_reading WHERE id = ? LIMIT 1;');
+  $sth->execute([intval($id)]);
+  echo json_encode($sth->fetchAll(PDO::FETCH_CLASS)[0]);
+});
+
+
+$app->run();
